b9e98029c5adcb1619f7ec96171491d6cac1e338
sandeepa amcp <sandeepaamcp@gmail.com>
2018-05-09 23:58:54 +0530

    bug fixes basic auth

--- app.js
+++ app.js
@@ -35,31 +35,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-function auth(req,res,next){
-  console.log(req.headers);
-
+function auth (req, res, next) {
+  console.log(req.headers.authorization);
   var authHeader = req.headers.authorization;
-
-  if(!authHeader){
-    var err = new Error("You are not authenticated!");
-    res.setHeader('WWW-Authenticate','basic');
-    res.status = 401;
-    return next(err);
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
   }
-
-  var auth = new Buffer(authHeader.split(' ')[1],"base64").toString().split(":");
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username=="admin" && password=="password"){
-    next();
-  }
-
-  else{
-    var err = new Error("You are not authenticated!");
-    res.setHeader('WWW-Authenticate','basic');
-    res.status = 401;
-    return next(err);
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
   }
 }
 
