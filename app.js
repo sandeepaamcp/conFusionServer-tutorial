d09cbe287892d6dbcc25838d0ad8cd7457882135
sandeepa amcp <sandeepaamcp@gmail.com>
2018-05-15 15:11:29 +0530

    passport plugin

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -44,27 +46,23 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
+function auth (req, res, next) {
+  console.log(req.user);
 
-  if (!req.session.user) {
-
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-
+    res.setHeader('WWW-Authenticate', 'Basic');                          
+    err.status = 401;
+    next(err);
   }
   else {
-    if (req.session.user == 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
